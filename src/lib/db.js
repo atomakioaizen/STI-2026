7a9fb8828a7a969e85a567cbca47d8e7c2b45d56
atomakioaizen <aldreamjamion@gmail.com>
2026-07-16 02:13:42 +0800

    Added fallback options to PrismaClient constructor to fix Vercel static build compilation

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,14 @@ import { PrismaClient } from '@prisma/client';
 const globalForPrisma = globalThis;
 
 function createPrismaClient() {
-  return new PrismaClient();
+  const url = process.env.DATABASE_URL || 'postgresql://mock:mock@localhost:5432/mock';
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url
+      }
+    }
+  });
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
